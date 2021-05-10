90f033f8e63f90469f250cc4e09d10ffb2fc7ac5
RishabhKatiyar <rishabh.katiyar@outlook.com>
2021-05-10 14:14:16 +0530

    Initial commit

---
+++ users/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Student
+
+
+
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_student', 'is_teacher', 'password1', 'password2')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_student', 'is_teacher', 'password')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    list_display = ['email', 'username', 'is_student', 'is_teacher']
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
+# admin.site.register(User)
+admin.site.register(Student)
